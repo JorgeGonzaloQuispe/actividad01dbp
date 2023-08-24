ebc8a25a350661031a03c8db22400a690c0099b8
Jorge Gonzalo Quispe Vargas <jorge.quispe@utec.edu.pe>
2023-08-24 02:53:17 -0500

    version 1.4 -> 2 get, 1 patch, 1 put

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ with open('lista.json','r') as json_file:
     animes_data=json.load(json_file)
 
 # METODO GET
-# ruta para obtener la lista de animes
 @app.route('/anime',methods=['GET'])
 def get_nombres_animes():
     nombres_animes=[f"{anime['id']}.{anime['titulo']}" for anime in animes_data["animes"]]
     response = "\n".join(nombres_animes)
     return response, 200,{'Content-Type': 'text/plain; charset=utf-8'}
 
-#METODO GET_PARTE2
+# METODO GET_PARTE2
 @app.route('/anime/<int:id>',methods={'GET'})
 def get_info_anime(id):
     anime=next((anime for anime in animes_data["animes"] if anime["id"]==id),None)
@@ -29,16 +28,16 @@ def get_info_anime(id):
         return response, 200, {'Content-Type': 'text/plain; charset=utf-8'}
     return response, 200,{'Content-Type': 'text/plain; charset=utf-8'}
 
-# METODO PATCH
+# METODO PATCH changes partial
 @app.route('/anime/<int:id>', methods=['PATCH'])
 def short_edition(id):
     anime = next((anime for anime in animes_data["animes"] if anime["id"] == id), None)
     response = "Anime no encontrado\nERROR 404"
     if anime != None:
         datosanime = request.json
-        for i, value in datosanime.items():
+        for i, dato in datosanime.items():
             if i in anime:
-                anime[i] = value
+                anime[i] = dato
         informacion = [f"Titulo:{anime['titulo']}"
                        f"\nID:{anime['id']} "
                        f"\nPuntaje:{anime['puntaje']}"
@@ -46,6 +45,30 @@ def short_edition(id):
                        f"\nSeason:{anime['season']}"
                        f"\nGeneros:{anime['generos']}"]
         response = "".join(informacion)
+        return response, 200, {'Content-Type': 'text/plain; charset=utf-8'}
+    return response, 200, {'Content-Type': 'text/plain; charset=utf-8'}
+
+# METODO PUT changes all or replace it
+@app.route('/anime/<int:id>', methods=['PUT'])
+def complete_edition(id):
+    anime = next((anime for anime in animes_data["animes"] if anime["id"] == id), None)
+    response = "Anime no encontrado\nERROR 404"
+    numero=0
+    if anime != None:
+        datosanime = request.json
+        for i, dato in datosanime.items():
+            numero=numero+1
+            if i in anime:
+                anime[i] = dato
+        informacion = [f"Titulo:{anime['titulo']}"
+                       f"\nID:{anime['id']} "
+                       f"\nPuntaje:{anime['puntaje']}"
+                       f"\nTipo:{anime['tipo']}"
+                       f"\nSeason:{anime['season']}"
+                       f"\nGeneros:{anime['generos']}"]
+        response = "".join(informacion)
+        if numero !=6:
+            return "sin cambios"
         return response, 200, {'Content-Type': 'text/plain; charset=utf-8'}
     return response, 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
