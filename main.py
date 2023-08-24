7bf1b4260f98f743a8239fed0d41f195ffa0c673
Jorge Gonzalo Quispe Vargas <jorge.quispe@utec.edu.pe>
2023-08-24 00:58:34 -0500

    version 1.2 se concluye ambos get

--- main.py
+++ main.py
@@ -8,11 +8,28 @@ with open('lista.json','r') as json_file:
 
 # METODO GET
 # ruta para obtener la lista de animes
-@app.route('/animes',methods=['GET'])
+@app.route('/anime',methods=['GET'])
 def get_nombres_animes():
     nombres_animes=[f"{anime['id']}.{anime['titulo']}" for anime in animes_data["animes"]]
     response = "\n".join(nombres_animes)
     return response, 200,{'Content-Type': 'text/plain; charset=utf-8'}
-if __name__=='__main__':
-    app.run(debug=True)
 
+#METODO GET_PARTE2
+@app.route('/anime/<int:id>',methods={'GET'})
+def get_info_anime(id):
+    anime=next((anime for anime in animes_data["animes"] if anime["id"]==id),None)
+    response = "Anime no encontrado\nERROR 404"
+    if anime:
+        informacion=[f"Titulo:{anime['titulo']}"
+                     f"\nID:{anime['id']} "
+                     f"\nPuntaje:{anime['puntaje']}"
+                     f"\nTipo:{anime['tipo']}"
+                     f"\nSeason:{anime['season']}"
+                     f"\nGeneros:{anime['generos']}"]
+        response="".join(informacion)
+        return response, 200, {'Content-Type': 'text/plain; charset=utf-8'}
+    return response, 200,{'Content-Type': 'text/plain; charset=utf-8'}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
